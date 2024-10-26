058519050e097f14881a4fdcc54b76ec52719637
Mr-Ojii <okaschan@gmail.com>
2024-10-26 22:44:52 +0900

    メモリリークの修正

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,19 +1,21 @@
 #include "Config.hpp"
+#include <algorithm>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <algorithm>
 
 namespace APP_NAME {
 Config::Config() {
-  std::filesystem::path toml_path =
-      std::filesystem::path(SDL_GetBasePath()) / "config.toml";
+  char *base = SDL_GetBasePath();
+  std::filesystem::path toml_path = std::filesystem::path(base) / "config.toml";
+  SDL_free(base);
   if (std::filesystem::exists(toml_path))
     toml = toml::parse_file(toml_path.string());
 }
 Config::~Config() {
-  std::filesystem::path toml_path =
-      std::filesystem::path(SDL_GetBasePath()) / "config.toml";
+  char *base = SDL_GetBasePath();
+  std::filesystem::path toml_path = std::filesystem::path(base) / "config.toml";
+  SDL_free(base);
   std::ofstream ofs(toml_path);
   if (!ofs)
     return;
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include "Depends.hpp"
 #include "Window.hpp"
 #include <cstdlib>
+#include <filesystem>
 #include <format>
 #include <iostream>
 
 int main() {
-  std::filesystem::current_path(SDL_GetBasePath());
+  {
+    char *base = SDL_GetBasePath();
+    std::filesystem::current_path(base);
+    SDL_free(base);
+  }
   SDL_SetHint(SDL_HINT_TRACKPAD_IS_TOUCH_ONLY, "1");
   int sdl_flags =
       SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_TIMER;
@@ -38,7 +43,8 @@ int main() {
     SDL_ShowSimpleMessageBox(
         SDL_MESSAGEBOX_ERROR, app_name,
         std::format("Exception has occured.\nFailed to parse toml.\n({}, {})",
-                    e.what(), e.source().path->c_str()).c_str(),
+                    e.what(), e.source().path->c_str())
+            .c_str(),
         nullptr);
   } catch (const std::exception &e) {
     SDL_ShowSimpleMessageBox(
@@ -46,9 +52,9 @@ int main() {
         std::format("Exception has occured.\nsorry...\n({})", e.what()).c_str(),
         nullptr);
   } catch (...) {
-    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, app_name,
-                             std::format("Exception has occured.\nsorry...\n").c_str(),
-                             nullptr);
+    SDL_ShowSimpleMessageBox(
+        SDL_MESSAGEBOX_ERROR, app_name,
+        std::format("Exception has occured.\nsorry...\n").c_str(), nullptr);
   }
   Mix_CloseAudio();
   return 0;
